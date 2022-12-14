ea2b0b81bfe07f13f957da052657407010cd8eb9
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-14 14:27:59 +0900

    Update share_form.js

--- js/share_form.js
+++ js/share_form.js
@@ -1,11 +1,12 @@
 const shareForm = document.getElementById('shareform');
 
+/*
 shareForm.onchange = () => {
 	if(document.getElementById('genre').selectedIndex !== 0 && document.getElementById('textarea').value !== ""){
 		submit.disabled = false;
 	}else submit.disabled = true;
 };
-
+*/
 
 
 const maxChar = 400;
