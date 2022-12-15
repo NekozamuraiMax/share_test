a6d9036c2af4c348cda04c8d3f5408e2917ef6a7
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-15 10:59:47 +0900

    Update share_initialize.js

--- js/share_initialize.js
+++ js/share_initialize.js
@@ -1,7 +1,5 @@
 const id = "1657662321-GMLZ3Qpv";
-window.alert('debug start.');
-
-	window.onload = function(e){
+window.onload = function(e){
 	liff.init({
 		liffId: id
 	}).then(() =>{
@@ -48,22 +46,22 @@ const key = params.get('key');
 
 $(function(){	
 	$('form').submit(function(){
-		/*
+		
 		const genre = document.getElementById("genre").value;
-		const date  = document.getElementById("datepicker").value;
-		const time  = document.getElementById("scheduled-time").value;
 		const freetxt= document.getElementById("textarea").value;
 		let message="None. This is not message.";
 		
-		if(genre==='reserve'){
-			message = '[申請内容]予定の追加\n'+'[指定日]'+date+'\n'+'[時間]'+time+'\n'+'[伝達事項]\n'+freetxt+ '\n\n上記の内容で申請しました。';
-		}else if(genre==='cancel'){
-			message = '[申請内容]キャンセル\n'+'[指定日]'+date+'\n'+'[伝達事項]\n'+freetxt+ '\n\n上記の内容で申請しました。';
-		}else if(genre==='change'){
-			message = '[申請内容]利用時間の変更\n'+'[指定日]'+date+'\n'+'[時間]'+time+'\n'+'[伝達事項]\n'+freetxt+ '\n\n上記の内容で申請しました。';
-		}*/
-		sendText('test success.');
+		if(genre==='day'){
+			message = '[放課後デイサービスへのご相談・ご意見]\n\n' + freetxt;
+		}else if(genre==='service'){
+			message = '[他のサービスに関するご相談]\n\n' + freetxt';
+		}else if(genre==='care'){
+			message = '[子育てについてのご相談]\n\n' + freetxt;
+		}else if(genre==='else'){
+			message = '[その他]\n\n' + freetxt
+		}
+		
+		sendText(message);
 		return false;
 	});
 });
-window.alert('debug end.');
